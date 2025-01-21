1b9835140f98c13aeed5c7e31e58804ee3d86062
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-01-21 16:49:12 +0000

    edit svgs
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,6 @@ import Install from "./Install.jsx";
 import Demo from "./Demo.jsx";
 import About from "./About.jsx";
 import Footer from "./Footer.jsx";
-// import installIcon from "./assets/gravity-ui--arrow-shape-down-to-line.svg";
-// import githubIcon from "./assets/octicon--mark-github-24.svg";
-// import communityIcon from "./assets/gravity-ui--persons.svg";
-// import hardwareIcon from "./assets/gravity-ui--gear.svg";
-// import llamaMascot from "./assets/ramalama-logo-llama-only.svg";
-// import aboutGraphic1 from "./assets/ramalama-about-graphic-1.svg";
-// import aboutGraphic2 from "./assets/ramalama-about-graphic-2.svg";
-// import aboutGraphic3 from "./assets/ramalama-about-graphic-3.svg";
-// import aboutGraphic4 from "./assets/ramalama-about-graphic-4.svg";
 import { StrictMode } from "react";
 
 /* -------------------------------- Main App Function -------------------------------- */
