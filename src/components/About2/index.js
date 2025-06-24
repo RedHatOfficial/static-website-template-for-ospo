1131da3d22e92d99f46e76b17e60e045123b0461
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-06-24 14:56:39 +0100

    add placeholder text
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/About2/index.js
+++ src/components/About2/index.js
@@ -5,34 +5,33 @@ export default function About2() {
       <div className={`${styles.showForMobile} ${styles.about} ${styles.aboutViewport} ${styles.blueTheme}`} id="about">
         <div className={styles.aboutGrid}>
                       <div className={styles.aboutSection1}>
-                        <h1 className={styles.aboutHeader}>Cloud Orientated</h1>
+                        <h1 className={styles.aboutHeader}>Header 1</h1>
                         <p className={styles.aboutText} role="paragraph">
-                          The ability to quickly deploy and run the broker as a container
-                          image
+                          This is where the text goes for the first section
                         </p>
                       </div>
                       <div className={styles.aboutSection2}>
-                        <h1 className={styles.aboutHeader}>Kubernetes</h1>
+                        <h1 className={styles.aboutHeader}>Header 2</h1>
                         <p className={styles.aboutText} role="paragraph">
-                          The ability to run a Kubernetes native container on Kubernetes
+                          This is where the text goes for the second section
                         </p>
                       </div>
                       <div className={styles.aboutSection3}>
-                        <h1 className={styles.aboutHeader}>Operator</h1>
+                        <h1 className={styles.aboutHeader}>Header 3</h1>
                         <p className={styles.aboutText} role="paragraph">
-                          The operator based solution to manage the Kubernetes deployment
+                          This is where the text goes for the third section
                         </p>
                       </div>
                       <div className={styles.aboutSection4}>
-                        <h1 className={styles.aboutHeader}>Examples</h1>
+                        <h1 className={styles.aboutHeader}>Header 4</h1>
                         <p className={styles.aboutText} role="paragraph">
-                          A suite of examples to demonstrate each feature
+                          This is where the text goes for the fourth section
                         </p>
                       </div>
                       <div className={styles.aboutSection5}>
-                        <h1 className={styles.aboutHeader}>CI/CD</h1>
+                        <h1 className={styles.aboutHeader}>Header 5</h1>
                         <p className={styles.aboutText} role="paragraph">
-                          Continuous intergration and deployment for quick development
+                          This is where the text goes for the fifth section
                         </p>
                       </div>
                     </div>
--- src/components/Welcome2/index.js
+++ src/components/Welcome2/index.js
@@ -12,64 +12,60 @@ export default function Welcome2() {
         <div className={styles.welcomeInfo}>
           <img className={styles.welcomeLogo} src={welcomeLogo} />
           <img className={styles.welcomeLogoDark} src={welcomeLogoDark} />
-          <h2 className={styles.welcomeH2}>
-            A collection of container images that provide a way to deploy the
-            Apache ActiveMQ Artemis Broker on Kubernetes
-          </h2>
+          <h2 className={styles.welcomeH2}>H2 text goes here</h2>
           <div className={styles.buttonGroup}>
             <button
               className={styles.staticButton}
-              onClick={() => history.push("/arkmq-org.github.io/docs/getting-started/quick-start")}
+              onClick={() => history.push("#")}
             >
-              Quick Start Guide here
+              Button 1
             </button>
             <button
               className={styles.staticButton}
-              onClick={() => history.push("/arkmq-org.github.io/docs/contribute")}
+              onClick={() => history.push("#")}
             >
-              Contribute to the project
+              Button 2
             </button>
 
             <button
               className={styles.staticButton}
-              onClick={() => history.push("/arkmq-org.github.io/docs/")}
+              onClick={() => history.push("#")}
             >
-              View documentation here
+              Button 3
             </button>
           </div>
           <div className={`${styles.aboutGrid} ${styles.hideForMobile}`}>
-                    <div className={styles.aboutSection1}>
-                      <h1 className={styles.aboutHeader}>Cloud Orientated</h1>
-                      <p className={styles.aboutText} role="paragraph">
-                        The ability to quickly deploy and run the broker as a container
-                        image
-                      </p>
-                    </div>
-                    <div className={styles.aboutSection2}>
-                      <h1 className={styles.aboutHeader}>Kubernetes</h1>
-                      <p className={styles.aboutText} role="paragraph">
-                        The ability to run a Kubernetes native container on Kubernetes
-                      </p>
-                    </div>
-                    <div className={styles.aboutSection3}>
-                      <h1 className={styles.aboutHeader}>Operator</h1>
-                      <p className={styles.aboutText} role="paragraph">
-                        The operator based solution to manage the Kubernetes deployment
-                      </p>
-                    </div>
-                    <div className={styles.aboutSection4}>
-                      <h1 className={styles.aboutHeader}>Examples</h1>
-                      <p className={styles.aboutText} role="paragraph">
-                        A suite of examples to demonstrate each feature
-                      </p>
-                    </div>
-                    <div className={styles.aboutSection5}>
-                      <h1 className={styles.aboutHeader}>CI/CD</h1>
-                      <p className={styles.aboutText} role="paragraph">
-                        Continuous intergration and deployment for quick development
-                      </p>
-                    </div>
-                  </div>
+            <div className={styles.aboutSection1}>
+              <h1 className={styles.aboutHeader}>Header 1</h1>
+              <p className={styles.aboutText} role="paragraph">
+                This is where the text goes for the first section
+              </p>
+            </div>
+            <div className={styles.aboutSection2}>
+              <h1 className={styles.aboutHeader}>Header 2</h1>
+              <p className={styles.aboutText} role="paragraph">
+                This is where the text goes for the second section
+              </p>
+            </div>
+            <div className={styles.aboutSection3}>
+              <h1 className={styles.aboutHeader}>Header 3</h1>
+              <p className={styles.aboutText} role="paragraph">
+                This is where the text goes for the third section
+              </p>
+            </div>
+            <div className={styles.aboutSection4}>
+              <h1 className={styles.aboutHeader}>Header 4</h1>
+              <p className={styles.aboutText} role="paragraph">
+                This is where the text goes for the fourth section
+              </p>
+            </div>
+            <div className={styles.aboutSection5}>
+              <h1 className={styles.aboutHeader}>Header 5</h1>
+              <p className={styles.aboutText} role="paragraph">
+                This is where the text goes for the fifth section
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
