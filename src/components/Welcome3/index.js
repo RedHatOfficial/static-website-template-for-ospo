a54608b1e8e50e3d67562eef43b79eed1032f79f
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-06-24 16:05:14 +0100

    add welcome section 3 and about section 3 fix buttons on welcome section 2
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

---
+++ src/components/Welcome3/index.js
@@ -0,0 +1,30 @@
+import styles from "./styles.module.css";
+import welcomeLogo from "@site/static/img/logo-text.png";
+import welcomeLogoSmall from "@site/static/img/logo-text.png";
+
+export default function Welcome3() {
+  return (
+    <div className={`${styles.welcome} ${styles.viewport}`}>
+      <div className={styles.welcomeInfo}>
+        <h1 className={styles.welcomeH1}>Welcome to</h1>
+        <img className={styles.welcomeLogo} width="75%" src={welcomeLogo}></img>
+        <img className={styles.welcomeLogoSmall} width="100%" src={welcomeLogoSmall}></img>
+        <div className={styles.buttonGroup}>
+          <button className={styles.staticButtonBluesky}>
+            <a href="#" target="_blank">
+              Button 1
+            </a>
+          </button>
+          <button className={styles.staticButtonDiscord}>
+            {" "}
+            <a href="#" target="_blank">Button 2</a>
+          </button>
+          <button className={styles.staticButton}>
+            {" "}
+            <a href="#" target="_blank">Button 3</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import Layout from '@theme/Layout';
 
 import Welcome from '@site/src/components/Welcome'
 import Welcome2 from '@site/src/components/Welcome2'
+import Welcome3 from '@site/src/components/Welcome3'
 import Install from '@site/src/components/Install'
 import Demo from '@site/src/components/Demo'
 import About from '@site/src/components/About'
 import About2 from '@site/src/components/About2'
+import About3 from '@site/src/components/About3'
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -17,10 +19,12 @@ export default function Home() {
       <main>
         <Welcome />
         <Welcome2 />
+        <Welcome3 />
         <Install />
         <Demo />
         <About />
         <About2 />
+        <About3 />
       </main>
     </Layout>
   );
