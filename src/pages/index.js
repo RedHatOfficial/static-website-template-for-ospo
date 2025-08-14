71a4f7f75c62a88c0c585c86432cd0831739e6a7
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-08-14 11:16:18 +0100

    re-sync disconnected fork with new changes - add patternfly - create form so that associates can choose different components that they feel like will fit their use case
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,17 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
-import Welcome from '@site/src/components/Welcome'
-import Welcome2 from '@site/src/components/Welcome2'
-import Welcome3 from '@site/src/components/Welcome3'
-import Install from '@site/src/components/Install'
-import Demo from '@site/src/components/Demo'
-import About from '@site/src/components/About'
-import About2 from '@site/src/components/About2'
-import About3 from '@site/src/components/About3'
+import Form from "@site/src/components/Form";
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <main>
-        <Welcome />
-        <Welcome2 />
-        <Welcome3 />
-        <Install />
-        <Demo />
-        <About />
-        <About2 />
-        <About3 />
+        <Form />
       </main>
     </Layout>
   );
